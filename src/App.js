cd08fb6fe37d400d628cf8d3fb3d262b235b8dec
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-07 21:30:57 +0300

    starting todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todoList: [],
+    keyCodes: {
+      enter: 13,
+      space: 32
+    }
+  };
+
+  check;
+
+  addTodoItem = ({ keyCode, target }) => {
+    if (keyCode !== this.state.keyCodes.enter) return;
+
+    const todo = target.value;
+    const todoObj = { id: Math.random(), item: todo, done: false };
+
+    this.setState(prevState => ({
+      todoList: prevState.todoList.concat(todoObj)
+    }));
+
+    target.value = "";
+  };
+
+  toggleTodoStatus = (todoId, { keyCode, target }) => {
+    if (keyCode !== this.state.keyCodes.space) return;
+
+    const newTodoList = this.state.todoList.map(todo => {
+      if (todo.id === todoId) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+
+    this.setState({ todoList: newTodoList });
+
+    target.classList.toggle("-done");
+  };
+
+  componentDidMount() {
+    this.refs.todoInputRef.focus();
+  }
+
+  render() {
+    const todos = this.state.todoList.map(todo => (
+      <li
+        className="m-todo__item"
+        key={todo.id}
+        tabIndex="0"
+        onKeyDown={e => this.toggleTodoStatus(todo.id, e)}
+      >
+        {todo.item}
+        <span>{todo.done}</span>
+      </li>
+    ));
+
+    return (
+      <div className="o-app">
+        <h1 className="o-app__header">Today's TO DO's</h1>
+        <div className="m-todo">
+          <input
+            ref="todoInputRef"
+            className="m-todo__input"
+            onKeyDown={this.addTodoItem}
+            name="todo"
+            id="todoInput"
+            type="text"
+            placeholder="What do you want to do today 🍓"
+          />
+          <ul className="m-todo__list">{todos}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
