6718c843c658198aab8f210472c214eab7eaf32b
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-08 21:35:29 +0300

    yeni oluşturulan reducer eklendi

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import todos from "./todoReducer";
+import activeFilter from './activeFilterReducer';
 
 export default combineReducers({
-  todos
+  todos,
+  activeFilter
 });
