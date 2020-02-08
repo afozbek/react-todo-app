aa1624772a3d6abba07b130f600919512e9550ba
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-08 12:40:48 +0300

    silme işlemleri gibi belirli feature lar getirildi

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from "react";
 class App extends React.Component {
   state = {
     todoList: [],
+    completedItems: 0,
     keyCodes: {
       enter: 13,
-      space: 32
+      space: 32,
+      left: 37,
+      up: 38,
+      right: 39,
+      down: 40
     }
   };
 
@@ -17,25 +22,97 @@ class App extends React.Component {
     const todoObj = { id: Math.random(), item: todo.trim(), done: false };
 
     this.setState(prevState => ({
-      todoList: prevState.todoList.concat(todoObj)
+      todoList: prevState.todoList.concat(todoObj),
+      completedItems: prevState.completedItems + 1
     }));
 
     target.value = "";
   };
 
-  toggleTodoStatus = (todoId, { keyCode, target }) => {
-    if (keyCode !== this.state.keyCodes.space) return;
+  todoKeyDownHandler = (todoId, { keyCode, target }) => {
+    if (Object.values(this.state.keyCodes).indexOf(keyCode) === -1) return;
 
+    if (keyCode === this.state.keyCodes.space) {
+      return this.toggleTodoStatus(todoId, target);
+    }
+
+    return this.focusElement(keyCode, target);
+  };
+
+  focusElement = (keyCode, target) => {
+    const { left, up, down, right } = this.state.keyCodes;
+    const todoItems = this.refs.todoListRef.children;
+    const firstTodoItem = todoItems[0];
+    const lastTodoItem = todoItems[todoItems.length - 1];
+
+    switch (keyCode) {
+      case left:
+      case up:
+        this.focusPreviousElement(target, lastTodoItem);
+        break;
+      case down:
+      case right:
+        this.focusNextElement(target, firstTodoItem);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  focusPreviousElement = (target, defaultEl) => {
+    const prevEl = target.previousElementSibling;
+
+    let nextFocusEl = prevEl;
+    if (!prevEl) {
+      nextFocusEl = defaultEl;
+    }
+
+    nextFocusEl.focus();
+  };
+
+  focusNextElement = (target, defaultEl) => {
+    const nextEl = target.nextElementSibling;
+
+    let nextFocusEl = nextEl;
+    if (!nextEl) {
+      nextFocusEl = defaultEl;
+    }
+
+    nextFocusEl.focus();
+  };
+
+  toggleTodoStatus = (todoId, target) => {
+    let completed = false;
     const newTodoList = this.state.todoList.map(todo => {
       if (todo.id === todoId) {
         todo.done = !todo.done;
+        completed = todo.done;
       }
       return todo;
     });
 
-    this.setState({ todoList: newTodoList });
+    this.setState(prevState => ({
+      todoList: newTodoList,
+      completedItems: completed
+        ? prevState.completedItems - 1
+        : prevState.completedItems + 1
+    }));
 
     target.classList.toggle("-done");
+  };
+
+  removeTodoItem = (todoId, e) => {
+    const filteredTodos = this.state.todoList.filter(
+      todo => todo.id !== todoId
+    );
+
+    this.setState(prevState => ({
+      todoList: filteredTodos,
+      completedItems: prevState.completedItems - 1
+    }));
+
+    this.refs.todoInputRef.focus();
   };
 
   componentDidMount() {
@@ -49,11 +126,37 @@ class App extends React.Component {
         aria-label={"You want to do: " + todo.item}
         key={todo.id}
         tabIndex="0"
-        onKeyDown={e => this.toggleTodoStatus(todo.id, e)}
+        onKeyDown={e => this.todoKeyDownHandler(todo.id, e)}
       >
-        {todo.item}
+        <p className="m-todo__desc">{todo.item}</p>
+        <button
+          className="m-todo__removeBtn"
+          onClick={e => this.removeTodoItem(todo.id, e)}
+        >
+          Delete Todo
+        </button>
       </li>
     ));
+
+    const todoList = (
+      <ul className="m-todo__list" ref="todoListRef">
+        {todos}
+      </ul>
+    );
+
+    const main =
+      todos.length > 0 ? (
+        <main className="o-app__main">
+          {todoList}
+          <footer className="o-app__footer">
+            <p>{this.state.completedItems} Items Left</p>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+            <button>Clear Completed</button>
+          </footer>
+        </main>
+      ) : null;
 
     return (
       <div className="o-app">
@@ -69,7 +172,8 @@ class App extends React.Component {
             type="text"
             placeholder="What do you want to do today 🍓"
           />
-          <ul className="m-todo__list">{todos}</ul>
+
+          {main}
         </div>
       </div>
     );
