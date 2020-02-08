725de3bfc2472dbdf63d1fc730e209069302b9b1
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-08 21:35:13 +0300

    activeFilterReducer oluşturuldu

---
+++ src/store/reducers/activeFilterReducer.js
@@ -0,0 +1,12 @@
+// FILTER REDUCER
+const activeFilterReducer = (state = "ALL", action) => {
+  switch (action.type) {
+    case 'CHANGE_ACTIVE_FILTER':
+      state = action.filter
+      return state;
+    default:
+      return state
+  }
+}
+
+export default activeFilterReducer;
