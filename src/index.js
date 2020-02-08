37dd1269c2db14ad7296ed763fcdffa31740d061
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-08 22:31:23 +0300

    redux için statement eklendi

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./main.scss";
 
-
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-   document.getElementById("root"));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
