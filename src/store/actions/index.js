b9ab98bc4267f90bb5563b998ab4724931848608
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-08 21:35:57 +0300

    yeni actionslar eklendi

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,27 @@
-export const addTodo = event => ({
+// TODOS
+export const addTodoItem = event => ({
   type: 'ADD_TODO',
   event
-})
+});
 
-export const removeTodo = (todoId, target) => ({
+export const removeTodoItem = (todoId, target) => ({
   type: 'REMOVE_TODO',
   todoId,
   target
-})
+});
+
+export const toggleTodoStatus = (todoId, target) => ({
+  type: "TOGGLE_TODO",
+  todoId,
+  target
+});
+
+export const clearCompletedTodos = () => ({
+  type: "CLEAR_COMPLETED_TODOS"
+});
+
+// STATUS
+export const changeActiveFilter = filter => ({
+  type: "CHANGE_ACTIVE_FILTER",
+  filter
+});
