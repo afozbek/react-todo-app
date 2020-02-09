8226e908c961783254bba529b0f840eb5282fdc6
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-09 13:06:04 +0300

    clear all filtresinde birden fazla action dispatch edildi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux';
 
 import { clearCompletedTodos, changeActiveFilter } from "../store/actions";
 
-const Footer = ({todoListLength, clearCompletedTodos, changeActiveFilter}) => {
+const Footer = ({todoListLength, clearTodos, changeFilter}) => {
   const setActiveFilter = (e) => {
     const filter = e.target.dataset.filter;
-    changeActiveFilter(filter);
+    changeFilter(filter);
   }
 
   return todoListLength > 0 ? (
@@ -16,22 +16,30 @@ const Footer = ({todoListLength, clearCompletedTodos, changeActiveFilter}) => {
       <button className="o-app__filterBtn" data-filter="ALL" onClick={setActiveFilter}>All</button>
       <button className="o-app__filterBtn" data-filter="ACTIVE" onClick={setActiveFilter}>Active</button>
       <button className="o-app__filterBtn" data-filter="COMPLETED" onClick={setActiveFilter}>Completed</button>
-      <button className="o-app__filterBtn" onClick={clearCompletedTodos}>Clear Completed</button>
+      <button className="o-app__filterBtn" onClick={clearTodos}>Clear Completed</button>
     </footer>
   ) : null;
 }
 
 Footer.propTypes = {
-  todoListLength: PropTypes.number.isRequired
+  todoListLength: PropTypes.number.isRequired,
+  clearTodos: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-  todoListLength : state.todos.length
+  todoListLength : state.todos.length,
 });
 
-const mapDispatchToProps = {
-  clearCompletedTodos,
-  changeActiveFilter
-}
+const mapDispatchToProps = dispatch => ({
+  clearTodos: () => {
+    dispatch(clearCompletedTodos());
+    dispatch(changeActiveFilter("ALL"));
+
+    // TODO: REF EKLENEBİLİR
+    document.querySelector(".m-todo__input").focus();
+  },
+  changeFilter: (filter) => dispatch(changeActiveFilter(filter))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Footer)
