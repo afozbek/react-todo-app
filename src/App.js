98cbf2976422077013a9c48a8fa93068397cd493
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-09 22:27:14 +0300

    comment eklendi

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.props.initTodoListState();
+    // DOWNLOAD LINK FOR USER DATA
     const downloadLink = `data:text/json;charset=utf-8,${encodeURIComponent(getLocalStorageTodoState(true))}`
 
     this.setState({ downloadLink });
