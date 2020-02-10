05005e2f4d63c00518995eeafba37627f26b524f
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-10 11:15:25 +0300

    redux store u redux-persist e bağlandı

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './store/reducers'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./main.scss";
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { store, persistedStore } from "./store/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
