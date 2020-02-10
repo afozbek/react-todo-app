8baac32ef2ff9ad3f70671f32acad1f14f99da53
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-10 11:41:44 +0300

    redux-persist kaldırıldı

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./main.scss";
 
-import { store, persistedStore } from "./store/configureStore";
+import { store } from "./store/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistedStore}>
       <App />
-    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
