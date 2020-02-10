f7c479acb6e6a9fe61d2640128dcd141008358f8
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-10 11:41:25 +0300

    redux-persist çıkardığı bir bug nedeniyle kaldırıldı

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,29 +1,11 @@
 import { createStore } from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-import { LOCALSTORAGE_TODO_STATE } from "./actions/types"
 import rootReducer from "./reducers";
 
-// DO WHAT CONFIG YOU WANT
-const persistConfig = {
-  key: LOCALSTORAGE_TODO_STATE,
-  storage,
-  stateReconciler: autoMergeLevel2,
-  whitelist: ["todos"]
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = createStore(
-  persistedReducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-const persistedStore = persistStore(store);
 
-export {
-  store,
-  persistedStore
-};
+export { store };
