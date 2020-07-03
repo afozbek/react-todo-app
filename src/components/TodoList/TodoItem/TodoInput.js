091ef4f105baa9cbd8f25641eeebb47ea9ff0a72
afozbek <furkanozbek1995@gmail.com>
2020-07-03 17:34:42 +0300

    todo lar eklenmeden önce trimlendi

--- src/components/TodoList/TodoItem/TodoInput.js
+++ src/components/TodoList/TodoItem/TodoInput.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { addTodoItem } from "../../../store/actions";
-import { keyCodes } from "../../../util";
+import { addTodoItem } from '../../../store/actions';
+import { keyCodes } from '../../../util';
 
 const TodoInput = ({ addTodoItem }) => {
   const inputKeyDownHandler = ({ target, keyCode }) => {
     // Simple validation. Delete if you want :)
-    if (keyCode !== keyCodes.enter || target.value.length < 2 || target.value.trim() === "") return;
+    if (
+      keyCode !== keyCodes.enter ||
+      target.value.length < 2 ||
+      target.value.trim() === ''
+    )
+      return;
 
-    addTodoItem(target.value);
+    addTodoItem(target.value.trim());
 
-    target.value = "";
-  }
+    target.value = '';
+  };
 
   return (
     <input
@@ -26,14 +31,14 @@ const TodoInput = ({ addTodoItem }) => {
       placeholder="What do you want to do today... "
     />
   );
-}
+};
 
 TodoInput.propTypes = {
-  addTodoItem: PropTypes.func.isRequired
+  addTodoItem: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = {
-  addTodoItem
+  addTodoItem,
 };
 
 export default connect(null, mapDispatchToProps)(TodoInput);
